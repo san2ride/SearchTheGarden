3cc35f1fed5000f85be9467dfb9e17eab4d3c174
Jason <sanchezjason@me.com>
2025-01-31 14:34:17 -0700

    GardenGridViewModel, performSearch, Constants

---
+++ TheAppGarden/TheAppGarden/Utilitys/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TheAppGarden
+//
+//  Created by Jason Sanchez on 1/31/25.
+//
+
+import Foundation
+
+struct Titles {
+    static let gardenTitle = "Garden"
+    static let gardenDetails = "GardenDetails"
+}
