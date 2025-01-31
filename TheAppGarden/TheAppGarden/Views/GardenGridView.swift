68d00c505c768376d32b558d6024139464136dcc
Jason <sanchezjason@me.com>
2025-01-31 14:16:34 -0700

    columns

--- TheAppGarden/TheAppGarden/Views/GardenGridView.swift
+++ TheAppGarden/TheAppGarden/Views/GardenGridView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct GardenGridView: View {
+        
+    let columns = [
+        GridItem(.adaptive(minimum: 100)),
+        GridItem(.adaptive(minimum: 100)),
+        GridItem(.adaptive(minimum: 100)),
+        GridItem(.adaptive(minimum: 100))
+    ]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
